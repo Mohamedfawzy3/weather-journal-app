e45a86cb6c92db2053f648a475cbe7284cca8904
Mohamedfawzy3 <92164783+Mohamedfawzy3@users.noreply.github.com>
2021-10-08 23:01:23 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+let Data = {};
+
+// Require Express to run server
+const express = require('express');
+const App = express();
+
+const bodyParser = require('body-parser');
+App.use(bodyParser.urlencoded({ extended: false }));
+App.use(bodyParser.json());
+const cors = require('cors');
+App.use(cors());
+// the place which server get data
+App.use(express.static('website'));
+//create server
+const port = 3000;
+const server = App.listen(port, listening);
+function listening() {
+  console.log(`server running on localhost: ${port}`);
+};
+// Post Route
+const data = [];
+App.post('/add', addData);
+function addData(request, response) {
+  projectData["date"] = request.body.date;
+  projectData["temp"] = request.body.temp;
+  projectData["content"] = request.body.content;
+  response.send(Data);
+}
+
+// Callback function to complete GET '/all'
+App.get('/all', getData);
+
+function getData(request, response) {
+  response.send(Data);
+}
